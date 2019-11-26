9758269d7ba5ef5787c5691809d1c2dad953a074
Alexey Sadykov <alexey.sadykov@altarix.ru>
2019-11-26 17:51:23 +0400

    migrate rn index file to typescript

--- packages/_mobile/index.ts
+++ packages/_mobile/index.ts
@@ -1,8 +1,10 @@
 import { AppRegistry } from 'react-native'
 import MobileLauncher from '@archy/mobile-launcher'
+import IntentLauncher from '@archy/mobile-intent'
 import { appBuilder } from '@archy/core'
 import { name as appName } from './app.json'
 
 appBuilder.use(MobileLauncher)
+appBuilder.use(IntentLauncher)
 
 AppRegistry.registerComponent(appName, () => appBuilder.launcher())
